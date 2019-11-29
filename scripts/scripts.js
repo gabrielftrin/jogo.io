7089c4f93baa16a70cbabc92a48b47c86efcb078
Gabriel Fernandes Trindade <mrgabrielfernandes@hotmail.com>
2019-11-29 16:59:46 -0300

    Andar na ufba

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,30 @@
 $(document).ready(function() {
 
+	setInterval(renderizarCena, 30);
+	
+	$(document).on("keydown", function(event) {
+
+		if(event.key == "ArrowDown") playery += velocidade;
+		else if(event.key == "ArrowUp") playery -= velocidade;
+		else if(event.key == "ArrowLeft") playerx -= velocidade;
+		else if(event.key == "ArrowRight") playerx += velocidade;
+	
+	});
+
+});
+
+var playerx = 380, playery = 240;
+var velocidade = 16;
+
+function renderizarCena() {
+
+	$("canvas").clearCanvas();
+
 	$("canvas").drawRect({
 		fillStyle: "#000",
-		x: 320,
+		x: 380,
 		y: 240,
-		width: 760,
+		width: 640,
 		height: 480
 	});
 
@@ -12,8 +32,16 @@ $(document).ready(function() {
 		source: 'imagens/ondina.png',
 		width: 760,
 		height: 480,		
-		x: 320,
+		x: 380,
 		y: 240,
 	});
-
-});
+	
+	$("canvas").drawImage({
+		source: 'imagens/aluno.png',
+		width: 70,
+		height: 100,
+		x: playerx,
+		y: playery
+	});
+	
+}
